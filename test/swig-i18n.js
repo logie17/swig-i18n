ba9c337cb88d27a68fe8e1c62f041e1f46934c6a
Aaron Dayalan <adayalan@shutterstock.com>
2015-08-06 14:20:58 -0400

    adding update translations function, so we can push changes without reinitializing.  also add test

--- test/swig-i18n.js
+++ test/swig-i18n.js
@@ -36,6 +36,23 @@ exports["basic usage find translation"] = function(test){
   test.done();
 };
 
+exports["basic usage find updated translation"] = function(test){
+
+  this.swig_i18n.init({ TAG_LOOKUP: { es: 'foo' } });
+  var template = '{% i18n TAG_LOOKUP %}Default text{% endi18n%}';
+
+  var expected = this.swig.render(template, {locals:{i18n:{language: 'es'}}});
+
+  test.equal(expected, 'foo');
+
+  this.swig_i18n.updateTranslations({ TAG_LOOKUP: { es: 'bar' } });
+
+  var updated = this.swig.render(template, {locals:{i18n:{language: 'es'}}});
+  test.expect(2);
+  test.equal(updated, 'bar');
+  test.done();
+};
+
 exports["basic usage find translation as a function"] = function(test){
 
   this.swig_i18n.init(function(){return { TAG_LOOKUP: { es: 'foo' } }});
